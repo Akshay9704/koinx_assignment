cd759a594cb0df3575691b817d07af060f97b6c6
Akshay Bhasin <107040611+Akshay9704@users.noreply.github.com>
2023-11-02 22:04:14 +0530

    Initial Commit

---
+++ src/Components/Container.js
@@ -0,0 +1,230 @@
+import React, { useState, useEffect } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
+import Form from 'react-bootstrap/Form';
+import australia from "../Images/aus_flag.png"
+import { Button } from 'react-bootstrap';
+import tick from '../Images/tick.png'
+
+const Container = () => {
+    const [selectedIncome, setSelectedIncome] = useState('$18,201 - $45,000');
+    const [taxRate, setTaxRate] = useState('Nil + 19% of excess over $18,200');
+    const [purchaseValue, setPurchaseValue] = useState("");
+    const [saleValue, setSaleValue] = useState("");
+    const [expenseValue, setExpenseValue] = useState("");
+    const [gainValue, setGainValue] = useState("");
+    const [discountValue, setDiscountValue] = useState("");
+    const [netGains, setNetGains] = useState("");
+    const [termButton, setTermButton] = useState('short');
+
+    const handleIncomeSelect = (selectedItem) => {
+        setSelectedIncome(selectedItem);
+        if (selectedItem === "$0 - $18,200") {
+            setTaxRate("0%")
+        } else if (selectedItem === "$18,201 - $45,000") {
+            setTaxRate("Nil + 19% of excess over $18,200")
+        } else if (selectedItem === "$45,001 - $120,000") {
+            setTaxRate("$5,092 + 32.5% of excess over $45,000")
+        } else if (selectedItem === "$120,000 - $180,000") {
+            setTaxRate("$29,467 + 37% of excess over $120,000")
+        } else setTaxRate("$51,667 + 45% of excess over 180,000")
+    }
+
+    const handleChange = (e) => {
+        if (e.target.id === "purchase") {
+            setPurchaseValue(e.target.value);
+        } else if (e.target.id === "sale") {
+            setSaleValue(e.target.value);
+        } else if (e.target.id === "expense") {
+            setExpenseValue(e.target.value);
+        }
+    }
+
+    const calculateGain = () => {
+        const purchaseAmount = parseFloat(purchaseValue);
+        const saleAmount = parseFloat(saleValue);
+        const expenseAmount = parseFloat(expenseValue);
+
+        if (isNaN(purchaseAmount) || isNaN(saleAmount) || isNaN(expenseAmount)) {
+            setGainValue("");
+        } else {
+            const amount = saleAmount - purchaseAmount - expenseAmount;
+            setGainValue(amount.toFixed(2));
+        }
+    }
+
+    const calculateDiscount = () => {
+        const gainAmt = parseFloat(gainValue);
+        if (gainAmt >= 0) {
+            const disAmt = gainAmt / 2;
+            setDiscountValue(disAmt.toFixed(2));
+        }
+    }
+
+    const calculateNetGains = () =>{
+        const gainAmt = parseFloat(gainValue);
+        const disAmt = parseFloat(discountValue);
+        const netAmt = gainAmt - disAmt;
+        setNetGains(netAmt);
+    }
+
+    useEffect(() => {
+        calculateGain();
+        calculateDiscount();
+    }, [purchaseValue, saleValue, expenseValue, gainValue]);
+
+    const handleTermClick = (term) => {
+        setTermButton(term);
+    };
+
+    return (
+        <>
+            <div className='container-box'>
+                <h1 className='container-title'>Free Crypto Tax Calculator Australia</h1>
+                <section className='container-section1'>
+                    <div className='financial_yr'>
+                        <p>Financial Year</p>
+                        <Dropdown>
+                            <Dropdown.Toggle className='financial-toggle'>
+                                FY 2023-24
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">FY 2023-24</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div className='country'>
+                        <p>Country</p>
+                        <Dropdown>
+                            <Dropdown.Toggle className='country-toggle'>
+                                <div className='country-toggle-items'>
+                                    <img src={australia} alt="aus_flag" />
+                                    Australia
+                                </div>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu className='flag_menu'>
+                                <Dropdown.Item className='flag_item' href="#/action-1">
+                                    <img src={australia} alt="aus_flag" />
+                                    Australia
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                </section>
+                <section className='container-section2'>
+                    <div className='crypto_price'>
+                        <Form.Label className='crypto_price_label'>Enter purchase price of Crypto</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="purchase"
+                            className='crypto_price_input'
+                            value={purchaseValue}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='crypto_sale'>
+                        <Form.Label className='crypto_sale_label'>Enter sale price of Crypto</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="sale"
+                            className='crypto_sale_input'
+                            value={saleValue}
+                            onChange={handleChange}
+                        />
+                    </div>
+                </section>
+                <section className='container-section3'>
+                    <div className='expenses'>
+                        <Form.Label className='expenses_label'>Enter your Expenses</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="expense"
+                            className='crypto_expenses_input'
+                            value={expenseValue}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='investment'>
+                        <p>Investment Type</p>
+                        <div className='invest_btn'>
+                            <div>
+                                <Button variant={termButton === 'short' ? 'outline-primary' : 'outline-secondary'} id="short" onClick={() => handleTermClick('short')}>Short Term{termButton === 'short' ? <img className='tick' src={tick} /> : <img className='tick_hide' src={tick} />}</Button>
+                                <p>&lt; 12 Months</p>
+                            </div>
+                            <div>
+                                <Button variant={termButton === 'long' ? 'outline-primary' : 'outline-secondary'} id="long" onClick={() => handleTermClick('long')}>Long Term{termButton === 'long' ? <img className='tick' src={tick} /> : <img className='tick_hide' src={tick} />}</Button>
+                                <p>&gt; 12 Months</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='container-section4'>
+                    <div className='income'>
+                        <p>Select Your Annual Income</p>
+                        <Dropdown>
+                            <Dropdown.Toggle className='income-toggle'>
+                                <div className='income-toggle-items'>
+                                    {selectedIncome}
+                                </div>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1" onClick={() => handleIncomeSelect('$0 - $18,200')}>
+                                    $0 - $18,200
+                                </Dropdown.Item>
+                                <Dropdown.Item href="#/action-1" onClick={() => handleIncomeSelect('$18,201 - $45,000')}>
+                                    $18,201 - $45,000
+                                </Dropdown.Item>
+                                <Dropdown.Item href="#/action-1" onClick={() => handleIncomeSelect('$45,001 - $120,000')}>
+                                    $45,001 - $120,000
+                                </Dropdown.Item>
+                                <Dropdown.Item href="#/action-1" onClick={() => handleIncomeSelect('$120,000 - $180,000')}>
+                                    $120,000 - $180,000
+                                </Dropdown.Item>
+                                <Dropdown.Item href="#/action-1" onClick={() => handleIncomeSelect('$180,001+')}>
+                                    $180,001+
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div className='tax'>
+                        <p>Tax Rate:</p>
+                        <p>{taxRate}</p>
+                    </div>
+                </section>
+                {termButton === "long" ? (
+                    <section className='container-section5'>
+                        <div className='gains'>
+                            <Form.Label className='gains_label'>Capital gains amount</Form.Label>
+                            <Form.Control
+                                type="text"
+                                className='gains_input'
+                                disabled={true}
+                                value={`$ ${gainValue}`}
+                            />
+                        </div>
+                        <div className='discount'>
+                            <Form.Label className='discount_label'>Discount for long term gains</Form.Label>
+                            <Form.Control
+                                type="text"
+                                className='discount_input'
+                                disabled={true}
+                                value={`$ ${discountValue}`}
+                            />
+                        </div>
+                    </section>
+                ) : (null)}
+                <section className='container-section6'>
+                    <div className='net_capital'>
+                        <h4>Net Capital gains tax amount</h4>
+                        <h2 className='net_capital_value'>{`$ ${netGains}`}</h2>
+                    </div>
+                    <div className='tax_pay'>
+                        <h4>The tax you need to pay*</h4>
+                        <h2 className='tax_pay_value'>$ 812.5</h2>
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
+
+export default Container
---
+++ src/Components/Header.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Logo from "../Images/Logo.png"
+import Menu from "../Images/menu.png"
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <div>
+            <nav className="navbar">
+                <ul className="logo">
+                    <img src={Logo} alt="logo" />
+                </ul>
+                <ul className="nav-list">
+                    <li>Features</li>
+                    <li>Exchanges</li>
+                    <li>How it works!</li>
+                    <li>Blog</li>
+                    <li>About Us</li>
+                    <div className="signin">
+                        <button>Sign In</button>
+                    </div>
+                </ul>
+                <ul className="menu" onClick={handleShow}>
+                    <img src={Menu} alt="menu" />
+                </ul>
+                <Offcanvas show={show} onHide={handleClose}>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title><img src={Logo} alt="logo" /></Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <ul className="nav-list-mobile">
+                            <li>Features</li>
+                            <li>Exchanges</li>
+                            <li>How it works!</li>
+                            <li>Blog</li>
+                            <li>About Us</li>
+                            <div className="signin-mobile">
+                                <button>Sign In</button>
+                            </div>
+                        </ul>
+                    </Offcanvas.Body>
+                </Offcanvas>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
